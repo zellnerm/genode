099f8ed47fad9f158b1b8a36ec6ffc8298badd6c
BugUser0815 <reisner.alex93@gmail.com>
2016-11-10 17:35:20 +0100

    Add vagrant support

---
+++ provision.sh
@@ -0,0 +1,14 @@
+
+#######################
+#
+# This is a provision script
+# it will be called once when the vagrant vm is first provisioned
+# If you have commands that you want to run always please have a
+# look at the bootstrap.sh script
+#
+# Contributor: Bernhard Blieninger
+######################
+sudo apt-get update -qq
+sudo apt-get install libncurses5-dev texinfo autogen autoconf2.64 g++ libexpat1-dev flex bison gperf cmake libxml2-dev libtool zlib1g-dev libglib2.0-dev make pkg-config gawk subversion expect git libxml2-utils syslinux xsltproc yasm iasl lynx -qq
+cd /vagrant/
+make
