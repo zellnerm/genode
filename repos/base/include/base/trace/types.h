39cca5031421caa7ecf2daeff37b5d9e3058baa7
BugUser0815 <reisner.alex93@gmail.com>
2016-12-15 09:56:39 +0100

    Add missing line after merge

--- repos/base/include/base/trace/types.h
+++ repos/base/include/base/trace/types.h
@@ -35,6 +35,7 @@ namespace Genode { namespace Trace {
 	struct CPU_info;
 	struct RAM_info;
 	struct SCHEDULER_info;
+	struct Threads;
 } }
 
 
@@ -79,6 +80,12 @@ struct Genode::Trace::Execution_time
 	Execution_time(unsigned long long value) : value(value) { }
 };
 
+struct Genode::Trace::Threads
+{
+	unsigned long id[10];
+	unsigned prio[10];
+	int n;
+};
 
 /**
  * Subject information
