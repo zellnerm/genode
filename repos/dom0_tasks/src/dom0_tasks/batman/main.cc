da48ddd571718ad8ed1332488863c5d04cb88c58
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-01-16 15:31:03 +0000

    Add another simple task

---
+++ repos/dom0_tasks/src/dom0_tasks/batman/main.cc
@@ -0,0 +1,8 @@
+#include <base/printf.h>
+
+int main(void){
+	for(int i=0;i<7;i++){
+		PINF("Na");
+	}
+	PINF("Batman!");
+}
---
+++ repos/dom0_tasks/src/dom0_tasks/batman/target.mk
@@ -0,0 +1,4 @@
+TRGET = hey
+SRC_CC = main.cc
+# libc to enforce dynamically linked binary
+LIBS = base libc
