8e26344c989567aaad9a49215a23e8cc0dce0e60
Alexander Weidinger <alexwegoo@gmail.com>
2017-06-06 11:52:24 +0200

    Revert "Merge remote-tracking branch 'origin/libmosquitto' into HEAD"
    This reverts commit 480be9ee65dfb7a45f2be4c6aa6f3858381b88ec, reversing
    changes made to 884614afd12e4b1afeb2aa4f52368d8169a9f34a.

--- repos/libports/src/test/libports/libmosquitto/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET = test-libmosquitto
-LIBS	 = libmosquitto
-SRC_CC = main.cc
-
-vpath main.cc $(PRG_DIR)/..
--- repos/mpct/src/mpct/target.mk
+++
@@ -1,4 +0,0 @@
-TARGET = mpct
-SRC_CC = main.cpp
-INC_DIR += /home/alex/Projects/genode/repos/libports/include/lwip/
-LIBS   = base libmosquitto stdcxx lwip pthread
